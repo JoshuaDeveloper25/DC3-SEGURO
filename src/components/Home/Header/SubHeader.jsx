dfd40955a9f021a0d6009f9dbcce96d8ddee0686
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2025-03-11 18:18:10 -0600

    Finished animations on subheader section

--- src/components/Home/Header/SubHeader.jsx
+++ src/components/Home/Header/SubHeader.jsx
@@ -1,10 +1,21 @@
 import { definitionsApp } from "@/src/data/dataHome";
 
+// External imports
+import { motion } from "framer-motion";
+
 const SubHeader = () => {
   return (
     <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-24">
-      {definitionsApp?.map((definitionApp) => (
-        <div
+      {definitionsApp?.map((definitionApp, index) => (
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.3,
+            delay: (index + 1) * 0.1,
+            ease: "linear",
+          }}
+          viewport={{ once: true }}
           className="p-8 bg-white dark:bg-[#24303f] rounded-lg shadow-lg transform transition-all duration-300 hover:shadow-xl hover:bg-[#0948730d] hover:bg-opacity-5 dark:hover:bg-[#0948731a] dark:hover:bg-opacity-10"
           key={definitionApp?.title}
         >
@@ -17,7 +28,7 @@ const SubHeader = () => {
           <p className="text-body-color dark:text-body-color-dark leading-relaxed">
             {definitionApp?.description}
           </p>
-        </div>
+        </motion.div>
       ))}
     </section>
   );
