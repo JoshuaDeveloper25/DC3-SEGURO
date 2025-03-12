b6adfd70f2d564ceba2904469b4ab5b3c164f281
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2025-03-11 19:00:45 -0600

    Added animation to contact page and finished landing page with its animations

--- src/components/Home/Contact/index.jsx
+++ src/components/Home/Contact/index.jsx
@@ -1,3 +1,8 @@
+"use client";
+
+// External imports
+import { motion } from "framer-motion";
+
 // React imports
 import Link from "next/link";
 
@@ -6,13 +11,33 @@ const Contact = () => {
     <section className="py-20 bg-gray-50 dark:bg-[#1a222c]" id="contact">
       <div className="container mx-auto px-4">
         <div className="text-center mb-16">
-          <h2 className="text-3xl font-bold text-[#1c2434] dark:text-white mb-4">
+          <motion.h2
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              duration: 0.8,
+              delay: 0.3,
+              ease: "linear",
+            }}
+            viewport={{ once: true }}
+            className="text-3xl font-bold text-[#1c2434] dark:text-white mb-4"
+          >
             Contacto
-          </h2>
+          </motion.h2>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-2xl mx-auto">
-          <div className="text-center transform transition-all duration-300 ease-in-out">
+          <motion.div
+            initial={{ opacity: 0, x: -200 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{
+              duration: 0.8,
+              delay: 0.3,
+              ease: "easeInOut",
+            }}
+            viewport={{ once: true }}
+            className="text-center transform transition-all duration-300 ease-in-out"
+          >
             <div className="text-[#094873] text-3xl mb-4">
               <i className="bi bi-envelope"></i>
             </div>
@@ -22,9 +47,19 @@ const Contact = () => {
             </h4>
 
             <p>dc3seguro@outlook.com</p>
-          </div>
+          </motion.div>
 
-          <div className="text-center transform transition-all duration-300 ease-in-out">
+          <motion.div
+            initial={{ opacity: 0, x: 200 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{
+              duration: 0.8,
+              delay: 0.3,
+              ease: "easeInOut",
+            }}
+            viewport={{ once: true }}
+            className="text-center transform transition-all duration-300 ease-in-out"
+          >
             <div className="text-[#094873] text-3xl mb-4">
               <i className="bi bi-phone"></i>
             </div>
@@ -38,7 +73,7 @@ const Contact = () => {
             </Link>
 
             <p>+52 442 784 5143</p>
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
