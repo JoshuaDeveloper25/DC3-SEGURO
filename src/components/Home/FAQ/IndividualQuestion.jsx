c44d6d1d29e5b19575a5137089a9f98681202bc0
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2025-03-12 13:20:55 -0600

    Added animation when opening accordion in faq section

--- src/components/Home/FAQ/IndividualQuestion.jsx
+++ src/components/Home/FAQ/IndividualQuestion.jsx
@@ -1,5 +1,5 @@
 // External imports
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 // Icons
 import { AddIcon, SubtractIcon } from "../../Icons";
@@ -7,7 +7,7 @@ import { AddIcon, SubtractIcon } from "../../Icons";
 const IndividualQuestion = ({ faq, isOpen, onToggle, index }) => {
   return (
     <motion.article
-      className="rounded-md border border-primary-2/50 p-4 dark:border-primary-9 dark:shadow-none md:p-6 xl:p-7.5"
+      className="rounded-md border border-primary-2/25 shadow-lg p-4 dark:border-primary-9 dark:shadow-none md:p-6 xl:p-7.5"
       initial={{ opacity: 0, y: 100 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{
@@ -28,7 +28,11 @@ const IndividualQuestion = ({ faq, isOpen, onToggle, index }) => {
           </h4>
         </div>
 
-        <div className="flex h-9 w-full max-w-9 items-center justify-center rounded-full border border-primary-3 dark:border-white">
+        <motion.div
+          className="flex h-9 w-full max-w-9 items-center justify-center rounded-full border border-primary-3 dark:border-white"
+          animate={{ rotate: isOpen ? 0 : 180 }} 
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+        >
           {!isOpen ? (
             <AddIcon
               fillColor={
@@ -42,18 +46,28 @@ const IndividualQuestion = ({ faq, isOpen, onToggle, index }) => {
               }
             />
           )}
-        </div>
+        </motion.div>
       </button>
 
-      {isOpen && (
-        <div className="mt-5 duration-200 ease-in-out">
-          <ul className="list-disc list-inside space-y-2 max-w-[830px] font-medium">
-            {faq?.answers?.map((answer) => (
-              <li key={answer}>{answer}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0, height: 0 }}
+            animate={{ opacity: 1, height: "auto" }}
+            exit={{ opacity: 0, height: 0 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className="overflow-hidden"
+          >
+            <div className="mt-5">
+              <ul className="list-disc list-inside space-y-2 max-w-[830px] font-medium">
+                {faq?.answers?.map((answer) => (
+                  <li key={answer}>{answer}</li>
+                ))}
+              </ul>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </motion.article>
   );
 };
